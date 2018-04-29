ce5baa5b2799bd677606098c52e63e0bbcecb0b1
Ayana Bando <ayanaflax@yahoo.com>
2018-04-29 00:15:31 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,8 +3,8 @@ class Backer
   attr_accessor :name, :backed_projects
 
   def initialize(name)
-    @backed_projects = []
     @name = name
+    @backed_projects = []
   end
 
   def back_project(project)
